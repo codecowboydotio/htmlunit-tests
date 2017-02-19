8c2dc73d9efce09788934007c0aa659d9ad1afea
scottorama1 <svankalken@yahoo.com.au>
2017-02-20 08:55:02 +1100

    made tap file

--- test.java
+++ test.java
@@ -25,31 +25,32 @@ public static void main(String[] args) throws Exception {
     String string = prop.getProperty("website_text");
 
 
+    System.out.println("1..2");
     HtmlPage currentPage = client.getPage(address);
     try {
     	WebAssert.assertTitleEquals(currentPage, title);
 	title_success = true;
 	}
     catch (AssertionError e) {
-	System.out.println(e);
+	System.out.println("not ok 1 - " + e);
 	System.exit(1);
 	}
     finally {
-	if (title_success==true) System.out.println("Title: Successfully Tested");
+	if (title_success==true) System.out.println("ok 1 - Title: Successfully Tested");
 	}
     try {
     	WebAssert.assertTextPresent(currentPage, string);
 	text_success = true;
 	}
     catch (AssertionError e) {
-	System.out.println(e);
+	System.out.println("not ok 2 - " + e);
 	System.exit(2);
 	}
     finally {
-	if (text_success) System.out.println("Text: Successfully Tested");
+	if (text_success) System.out.println("ok 2 - Text: Successfully Tested");
 	}
 
-	if (text_success && title_success) System.out.println("Success is 100%");
+	//if (text_success && title_success) System.out.println("ok 3 - Success is 100%");
 }
 
 }
